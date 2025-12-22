9de800d642849c148a1ae7b1a513628c80a6ce38
Óscar Arbáizar <oscararbaizarcid@gmail.com>
2025-12-22 13:29:17 +0100

    PT2 with automated testing + changes in settings

--- project/urls.py
+++ project/urls.py
@@ -17,16 +17,14 @@ Including another URLconf
 ### GLOBAL (project) ###
 
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Serve MEDIA in production (for this assignment/demo)
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     path('', include('relecloud.urls')),
 ]
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
